2f854330adfdca6586f300ccd59dbb621b30cf5c
Taku Ito <taku.ito@ColeLabMac.local>
2014-07-02 17:36:38 -0400

    commiting prepareEPI function in executeblocks module

--- executeBlocks.py
+++ executeBlocks.py
@@ -3,12 +3,16 @@
 
 sys.path.append('preprocbin')
 from workflows import *
+from run_shell_cmd import run_shell_cmd
 import os
 import glob
 
 def prepareMPRAGE(conf, logname):
 	"""
-	Input is each subject's subjConf, derived from general config
+	DESCRIPTION: Prepares and compiles anatomical MRPRAGE from raw DICOM files.  Not for HCP data use.  For use on a single subject.
+	PARAMETERS: 
+		conf - a subjectConfig file
+		logname - subject's log output filename
 	"""
 	os.chdir(conf.subjfMRIDir) #change working directory to fMRI directory
         
@@ -51,6 +55,50 @@ def prepareMPRAGE(conf, logname):
 
 
 
+def prepareEPI(conf, logname):
+	"""
+	DESCRIPTION: Converts fMRI data to AFNI format from raw DICOMs.
+	PARAMETERS: 
+		conf - a subjectConfig file
+		logname - subject's log output filename
+	"""
+	os.chdir(conf.subjfMRIDir)
+    
+    numRuns = len(conf.epi_series)
+    # Getting raw fMRI data folder names
+    # Modify: How to identify fMRI series directory in raw data [regular expression used to get series order correct]
+    ### Edit this, not very clean ###
+    rawDirRunList = []
+    for run in range(numRuns):
+    	# find epi series
+    	searchPath = glob.glob(conf.subjRawDataDir + conf.epi_series[run])[0]
+    	# append to rawDirRunList
+    	rawDirRunList.append(searchPath)
+    
+    print 'Raw data folder order:', rawDirRunList
+   
+    #For-loop for functions used across multiple runs (prior to run-concatenation)
+    for runNum in range(1,numRuns + 1):
+        print '--Run', runNum, '---'
 
+        fileFindString = '*.dcm'
+        runRawDir = rawDirRunList[runNum-1]
+        runRawFile = runRawDir + '/' + fileFindString
+        
+        # Sorting DICOM files (to make sure they will be read in the order they were collected in) using Freesurfer.
+        run_shell_cmd('dicom-rename ' + runRawFile + ' --o ' + conf.basedir + '/SortedDICOMs/Run' + str(runNum) + '/MR',logname)
+        
+        # Converting from DICOM to NIFTI to AFNI format using Freesurfer
+        run_shell_cmd('mri_convert ' + conf.basedir + '/SortedDICOMs/Run' + str(runNum) + '/*00001.dcm --in_type siemens --out_type nii epi_r' + str(runNum) + '.nii.gz',logname)
+        run_shell_cmd('rm epi_r' + str(runNum) + '+orig*',logname)
+        run_shell_cmd('3dcopy epi_r' + str(runNum) +'.nii.gz epi_r' + str(runNum),logname)
+        run_shell_cmd('rm epi_r' + str(runNum) + '.nii.gz',logname)
+        
+        # Remove sorted DICOMs
+        run_shell_cmd('rm -rf ' + conf.basedir + '/SortedDICOMs/Run' + str(runNum),logname)
+
+        # If numTRsToSkip > 0, remove the first couple TRs for every epi run
+        if conf.numTRsToSkip > 0:
+            run_shell_cmd('3dcalc -a epi_r' + str(runNum) + '+orig"[${numTRsToSkip}..$]" -expr ' + "'a' -prefix epi_r" + str(runNum) + ' -overwrite', logname)
 
 
